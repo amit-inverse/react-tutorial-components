126b06149819ec81d2a72e584992e39946001e29
Amit Kumar Das <amit.inverse@gmail.com>
2023-01-09 21:58:39 +0530

    Naive Component Approach

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 function App() {
-    return <div>Hi!</div>;
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
