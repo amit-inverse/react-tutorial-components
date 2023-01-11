965c762a2065ca5544b2b22a1b51e51f531b9d48
Amit Kumar Das <amit.inverse@gmail.com>
2023-01-11 21:35:24 +0530

    Passing Props - Solution

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import { faker } from '@faker-js/faker';
 import CommentDetail from './CommentDetail';
 
 function App() {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" />
-            <CommentDetail author="Alex" timeAgo="Today at 2:00AM" />
-            <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" />
+            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="Nice blog post" avatar={faker.image.avatar()} />
+            <CommentDetail author="Alex" timeAgo="Today at 2:00AM" content="I like the subject" avatar={faker.image.avatar()} />
+            <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" content="I like the writing" avatar={faker.image.avatar()} />
         </div>
     );
 }
