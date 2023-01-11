38a6a493dace069ecfa39b884e977be3b81d6c2d
Amit Kumar Das <amit.inverse@gmail.com>
2023-01-11 21:54:41 +0530

    Component Reuse

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ function App() {
     return (
         <div className="ui container comments">
             <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
                 <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="Nice blog post" avatar={faker.image.avatar()} />
             </ApprovalCard>
             <ApprovalCard>
